3d4d6d0d38acc93ef714de86088c23406211cb30
R. Dunbar Poor <rdpoor@gmail.com>
2020-11-11 14:34:10 -0800

    Remove utils_assert.h - use mu_config instead.

--- src/core/mu_sched.c
+++ src/core/mu_sched.c
@@ -35,7 +35,6 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <string.h> // memmove
-#include <utils_assert.h>
 
 // =============================================================================
 // local types and definitions
