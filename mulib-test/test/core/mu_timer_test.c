690a105eddf98aa6c72ef5f9e4dfccd880e04624
R. D. Poor <rdpoor@gmail.com>
2021-01-04 21:42:35 -0800

    Refactor mu_timer => mu_timer_one_shot and mu_timer_periodic

--- mulib-test/test/core/mu_timer_test.c
+++ mulib-test/test/core/mu_timer_test.c
@@ -55,7 +55,8 @@ static mu_spscq_item_t s_isr_queue_store[IRQ_QUEUE_SIZE];
 static mu_task_t s_task;
 static int s_task_call_count;
 
-static mu_timer_t s_timer;
+static mu_timer_t s_one_shot_timer;
+static mu_timer_t s_periodic_timer;
 
 // =============================================================================
 // public code
@@ -63,56 +64,62 @@ static mu_timer_t s_timer;
 void mu_timer_test() {
   setup();
 
-  ASSERT(mu_timer_init(&s_timer, &s_task) == &s_timer);
-  ASSERT(mu_timer_is_running(&s_timer) == false);
 
-  // start (from stopped mode) with repeat = false
-  ASSERT(mu_timer_start(&s_timer, 10, false) == &s_timer);
-  ASSERT(mu_timer_is_running(&s_timer) == true);
+  // one shot
+  ASSERT(mu_timer_one_shot(&s_one_shot_timer, &s_task) == &s_one_shot_timer);
+  ASSERT(mu_timer_is_running(&s_one_shot_timer) == false);
+
+  // start (from stopped mode)
+  ASSERT(mu_timer_start(&s_one_shot_timer, 10) == &s_one_shot_timer);
+  ASSERT(mu_timer_is_running(&s_one_shot_timer) == true);
 
   // not triggered yet...
   set_now(5);
   mu_sched_step();
   ASSERT(s_task_call_count == 0);
-  ASSERT(mu_timer_is_running(&s_timer) == true);
+  ASSERT(mu_timer_is_running(&s_one_shot_timer) == true);
 
   // trigger time has arrived
   set_now(10);
   mu_sched_step();
   ASSERT(s_task_call_count == 1);
-  ASSERT(mu_timer_is_running(&s_timer) == false);
+  ASSERT(mu_timer_is_running(&s_one_shot_timer) == false);
 
-  // trigger in 10 ticks from now, this time with repeat = true
-  ASSERT(mu_timer_start(&s_timer, 10, true) == &s_timer);
-  ASSERT(mu_timer_is_running(&s_timer) == true);
+  // periodic
+  ASSERT(mu_timer_periodic(&s_periodic_timer, &s_task) == &s_periodic_timer);
+  ASSERT(mu_timer_is_running(&s_periodic_timer) == false);
+
+  // trigger in 10 ticks from now with periodic timer
+  ASSERT(mu_timer_start(&s_periodic_timer, 10) == &s_periodic_timer);
+  ASSERT(mu_timer_is_running(&s_periodic_timer) == true);
 
   // not triggered yet...
   set_now(15);
   mu_sched_step();
   ASSERT(s_task_call_count == 1);
-  ASSERT(mu_timer_is_running(&s_timer) == true);
+  ASSERT(mu_timer_is_running(&s_periodic_timer) == true);
 
   // trigger time has arrived
   set_now(20);
   mu_sched_step();
   ASSERT(s_task_call_count == 2);
-  ASSERT(mu_timer_is_running(&s_timer) == true);  // b/c repeat = true
+  ASSERT(mu_timer_is_running(&s_periodic_timer) == true);  // b/c repeat = true
 
   // not triggered yet...
   set_now(25);
   mu_sched_step();
   ASSERT(s_task_call_count == 2);
-  ASSERT(mu_timer_is_running(&s_timer) == true);
+  ASSERT(mu_timer_is_running(&s_periodic_timer) == true);
 
   // trigger time has arrived
   set_now(30);
   mu_sched_step();
   ASSERT(s_task_call_count == 3);
-  ASSERT(mu_timer_is_running(&s_timer) == true);  // b/c repeat = true
+  ASSERT(mu_timer_is_running(&s_periodic_timer) == true);  // b/c repeat = true
 
   // stop timer
-  ASSERT(mu_timer_stop(&s_timer) == &s_timer);
-  ASSERT(mu_timer_is_running(&s_timer) == false);  // now stopped
+  ASSERT(mu_timer_stop(&s_periodic_timer) == &s_periodic_timer);
+  ASSERT(mu_timer_is_running(&s_periodic_timer) == false);  // now stopped
 
   // make sure it doesn't trigger...
   set_now(40);
@@ -120,16 +127,18 @@ void mu_timer_test() {
   ASSERT(s_task_call_count == 3);
 
   // restarting timer...
-  ASSERT(mu_timer_start(&s_timer, 10, true) == &s_timer);
-  ASSERT(mu_timer_is_running(&s_timer) == true);
+  ASSERT(mu_timer_start(&s_periodic_timer, 10) == &s_periodic_timer);
+  ASSERT(mu_timer_is_running(&s_periodic_timer) == true);
 
   set_now(45);
   mu_sched_step();
   ASSERT(s_task_call_count == 3);
 
+  ASSERT(mu_timer_stop(&s_periodic_timer) == &s_periodic_timer);
+
   // restart at t = 45
-  ASSERT(mu_timer_start(&s_timer, 10, false) == &s_timer);
-  ASSERT(mu_timer_is_running(&s_timer) == true);
+  ASSERT(mu_timer_start(&s_one_shot_timer, 10) == &s_one_shot_timer);
+  ASSERT(mu_timer_is_running(&s_one_shot_timer) == true);
 
   // doesn't trigger at t=50
   set_now(50);
@@ -140,7 +149,7 @@ void mu_timer_test() {
   set_now(55);
   mu_sched_step();
   ASSERT(s_task_call_count == 4);
-  ASSERT(mu_timer_is_running(&s_timer) == false);  // b/c repeat = false
+  ASSERT(mu_timer_is_running(&s_one_shot_timer) == false);  // b/c repeat = false
 }
 
 
