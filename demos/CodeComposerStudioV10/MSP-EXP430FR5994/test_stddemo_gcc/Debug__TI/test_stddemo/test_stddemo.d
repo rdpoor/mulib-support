1bd13873162208c73780193d61b8022b40b73ad7
R. D. Poor <rdpoor@gmail.com>
2021-05-22 06:13:00 -0700

    Add demo examples for MSP-EXP430FR5994

--- demos/CodeComposerStudioV10/MSP-EXP430FR5994/test_stddemo_gcc/Debug__TI/test_stddemo/test_stddemo.d
+++
@@ -1,9 +0,0 @@
-# FIXED
-
-test_stddemo/test_stddemo.obj: /Users/r/Projects/mulib-support/demos/shared/test_stddemo/test_stddemo.c
-test_stddemo/test_stddemo.obj: /Users/r/Projects/mulib-support/demos/shared/test_stddemo/test_stddemo.h
-
-/Users/r/Projects/mulib-support/demos/shared/test_stddemo/test_stddemo.c:
-
-/Users/r/Projects/mulib-support/demos/shared/test_stddemo/test_stddemo.h:
-
